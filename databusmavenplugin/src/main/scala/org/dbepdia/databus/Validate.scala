1d58053c69f610ba42e5071ea07a524b2a028a43
Termilion <robertbielinski@live.de>
2018-07-09 17:44:58 +0200

    cleaned Annotations

--- databusmavenplugin/src/main/scala/org/dbepdia/databus/Validate.scala
+++ databusmavenplugin/src/main/scala/org/dbepdia/databus/Validate.scala
@@ -2,8 +2,7 @@ package org.dbpedia.databus
 
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugin.MojoExecutionException
-import org.apache.maven.plugins.annotations.Mojo
-import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.plugins.annotations.{LifecyclePhase, Mojo, Parameter}
 
 
 /**
@@ -14,10 +13,8 @@ import org.apache.maven.plugins.annotations.Parameter
   * - get the public key from the webid
   * - get the private key from the config, generate a public key and compare to the public key
   *
-  *
-  * @phase validate
   */
-@Mojo(name = "validate")
+@Mojo(name = "validate", defaultPhase = LifecyclePhase.VALIDATE)
 class Validate extends AbstractMojo {
 
   @Parameter var maintainer:String = _
