439ab9b8eb0aeb78b8f9106292fd01fef131906d
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2018-07-09 08:43:10 +0200

    refactoring

--- databusmavenplugin/src/main/java/org/dbpedia/databus/DataIDCreation.java
+++ databusmavenplugin/src/main/java/org/dbpedia/databus/DataIDCreation.java
@@ -4,6 +4,7 @@ package org.dbpedia.databus;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -13,53 +14,46 @@ import java.io.IOException;
  * Goal which touches a timestamp file.
  *
  * @goal metadata
- * 
  * @phase compile
  */
-//@Mojo( name = "metadata")
+@Mojo( name = "metadata-create")
 public class DataIDCreation
-    extends AbstractMojo
-{
+        extends AbstractMojo {
+
+    @Parameter
+    private String maintainer;
+
+
     /**
      * Location of the file.
+     *
      * @parameter expression="${project.build.directory}"
      * @required
      */
     private File outputDirectory;
 
     public void execute()
-        throws MojoExecutionException
-    {
+            throws MojoExecutionException {
         File f = outputDirectory;
 
-        if ( !f.exists() )
-        {
+        if (!f.exists()) {
             f.mkdirs();
         }
 
-        File touch = new File( f, "touch.txt" );
+        File touch = new File(f, "touch.txt");
 
         FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
+        try {
+            w = new FileWriter(touch);
 
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
+            w.write("touch.txt");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
                     w.close();
-                }
-                catch ( IOException e )
-                {
+                } catch (IOException e) {
                     // ignore
                 }
             }
