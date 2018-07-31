2c980876f9840684d9209b16a9dad139088c9e1c
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2018-07-31 08:16:05 +0200

    added license

--- databusmavenplugin/src/main/scala/org/dbepdia/databus/FileAnalysis.scala
+++ databusmavenplugin/src/main/scala/org/dbepdia/databus/FileAnalysis.scala
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * databus-maven-plugin
+ * %%
+ * Copyright (C) 2018 Sebastian Hellmann (on behalf of the DBpedia Association)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 package org.dbpedia.databus
 
 import java.io._
@@ -88,6 +109,8 @@ class FileAnalysis extends AbstractMojo {
     val privateKey = HashAndSign.readPrivateKeyFile(privateKeyFile)
 
     val signatureBytes: Array[Byte] = HashAndSign.sign(privateKey, datafile);
+
+
     val signatureBase64 = new String(Base64.getEncoder.encode(signatureBytes))
     getLog.info(s"Signature: $signatureBase64")
 
