06b5b77eba8e546f599018a790af758024e1a8bb
Marvin <hofer@informatik.uni-leipzig>
2018-09-07 14:04:13 +0200

    download script

---
+++ download.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# TODO better cli
+
+for dataid in $*
+do 
+	# get dowload paths for dataid
+	paths=`rapper -i turtle -o ntriples $dataid 2> /dev/null \
+	| grep "<http://www.w3.org/ns/dcat#downloadURL>" \
+	| cut -d" " -f3 \
+	| sed "s/[<>]//g"`
+
+	for fileurl in $paths
+	do
+		# get artifactId , version and filename
+		filename=${fileurl##*/}
+		version=${fileurl%/*} 
+		artifactId=${version%/*}
+		version=${version##*/} 
+		artifactId=${artifactId##*/} # echo $artifactId $version
+
+		# mkdirs
+		mkdir -p $artifactId/$version
+
+		# download
+		wget -O - $fileurl 2> /dev/null \
+		| lbzcat \
+		| rapper -i ntriples - http://baseuri 2> /dev/null \
+		| lbzip2 > $artifactId/$version/$filename
+	done
+done
+
+
+
+
+
