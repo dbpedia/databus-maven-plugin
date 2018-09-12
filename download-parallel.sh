a2dc91129cf128a00e93f621fdbdaec00917d5e6
Markus Ackermann <ackermann@informatik.uni-leipzig.de>
2018-09-13 00:14:56 +0200

    adding actually intended version of script file

--- download-parallel.sh
+++ download-parallel.sh
@@ -1,34 +1,45 @@
-#! /bin/bash
+ #! /bin/bash
 
 set -euo pipefail
 
 # TODO better cli
 
-for dataid in $*
-do 
-    echo "downloading: $dataid"
-	# get dowload paths for dataid
-	paths=`rapper -i turtle -o ntriples $dataid 2> /dev/null \
-	| grep "<http://www.w3.org/ns/dcat#downloadURL>" \
-	| cut -d" " -f3 \
-	| sed "s/[<>]//g"`
 
-	for fileurl in $paths
-	do
-		# get artifactId , version and filename
-		filename=${fileurl##*/}
-		version=${fileurl%/*} 
-		artifactId=${version%/*}
-		version=${version##*/} 
-		artifactId=${artifactId##*/} # echo $artifactId $version
+process_link_cmd=$(cat <<'EOF'
 
-		# mkdirs
-		mkdir -p $artifactId/$version
+    # extract the first field, ommitting the backslash
+    url=$(echo "{}" | perl -ne '{ print $1 if /^([^ ]+?)\\/ }')
+    sinkpath=$(echo "{}" | awk '{ print $2 }')
 
-		# download
-		curl --fail --silent --show-error $fileurl 2> /dev/null \
-		| lbzcat \
-		| rapper -i ntriples - http://baseuri 2> /dev/null \
-		| lbzip2 > $artifactId/$version/$filename
-	done
-done
+    curl --fail --silent --show-error $url \
+	| lbzcat \
+	| rapper -i ntriples - http://baseuri 2> /dev/null \
+	| lbzip2 > $sinkpath
+EOF
+)
+
+collect_links_and_sinks() {
+
+    for dataid in $*; do 
+	    # get dowload paths for dataid
+	    links=`rapper -i turtle -o ntriples $dataid 2> /dev/null \
+	    | grep "<http://www.w3.org/ns/dcat#downloadURL>" \
+	    | cut -d" " -f3 \
+	    | sed "s/[<>]//g"`
+
+        for url in $links; do
+       		filename=${url##*/}
+		    version=${url%/*} 
+		    artifactId=${version%/*}
+		    version=${version##*/} 
+		    artifactId=${artifactId##*/} # echo $artifactId $version
+
+
+            echo "$url" "$artifactId/$version/$filename"
+        done
+    done
+}
+
+echo "||$process_link_cmd||"
+
+collect_links_and_sinks $* | parallel "$process_link_cmd"
