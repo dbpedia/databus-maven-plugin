9bd2f52753bad91cf3495fe96a007054d32faa9a
Markus Ackermann <ackermann@informatik.uni-leipzig.de>
2018-12-18 18:29:29 +0100

    minor code tweaks in script

--- download-parallel.sh
+++ download-parallel.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 
 process_link_cmd=$(cat <<'EOF'
 
-    # extract the first field, ommitting the backslash
+    # extract the first field, omitting the backslash
     url=$(echo "{}" | perl -ne '{ print $1 if /^([^ ]+?)\\/ }')
     sinkpath=$(echo "{}" | awk '{ print $2 }')
 
@@ -20,18 +20,18 @@ EOF
 
 collect_links_and_sinks() {
 
-    for dataid in $*; do 
-	    # get dowload paths for dataid
-	    links=`rapper -i turtle -o ntriples $dataid 2> /dev/null \
-	    | grep "<http://www.w3.org/ns/dcat#downloadURL>" \
+    for dataid in $*; do
+	    # get download paths for dataid
+	    links=`rapper -i turtle -o ntriples ${dataid} 2> /dev/null \
+	    | grep "<http://www.w3.org/ns/dcat#downloadurl>" \
 	    | cut -d" " -f3 \
 	    | sed "s/[<>]//g"`
 
-        for url in $links; do
+        for url in ${links}; do
        		filename=${url##*/}
-		    version=${url%/*} 
+		    version=${url%/*}
 		    artifactId=${version%/*}
-		    version=${version##*/} 
+		    version=${version##*/}
 		    artifactId=${artifactId##*/} # echo $artifactId $version
 
 
