cbb803cbf09e22338f0e468ac14ebc137dfb72ea
Root <kurzum@github.com>
2019-01-23 11:22:34 +0100

    some fixes in artifact names and rapandsort

--- dbpedia/rapandsort.sh
+++ dbpedia/rapandsort.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-ARTI=$1
+ARTIFACT=$1
+
 
 if [ ! -d "tmpfolder" ]; then
     mkdir -p tmpfolder
 fi
 
-if [ "$ARTI" = "common-metadata" ]; then
+# skip parent
+if [ "$ARTIFACT" = "common-metadata" ]; then
     exit
 fi
 
 LOG=tmpfolder/logfile
 echo -n "" > $LOG
 
-for file in */src/main/databus/*/*.ttl.bz2; do
+TMPFILE=tmpfolder/tmpfile
+
+# paths are now relative to the artifact dir
+for file in src/main/databus/*/*.ttl.bz2; do
         echo "processing $file ..." >> $LOG  ;
         lbzip2 -dc $file |\
 	rapper -i ntriples -O - - file 2>>$LOG |\
 	ascii2uni -a U 2>>$LOG  |\
 	LC_ALL=C sort --parallel=4 -u -T tmpfolder  |\
-        lbzip2 > tmpfile;
+        lbzip2 > $TMPFILE;
         echo "finished processing $file" >> $LOG ;
-        mv tmpfile $file;
+        mv $TMPFILE $file;
 done
