d3cf8a2e702528a16ff0ecf0ec7d8aae7883b2ce
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-08-15 08:28:09 +0200

    new sort test

--- src/test/scala/FileMetadataTest.scala
+++ src/test/scala/FileMetadataTest.scala
@@ -26,31 +26,32 @@ import org.scalatest.FunSuite
 
 class FileMetadataTest extends FunSuite  {
 
-  test("file parameters/metadata test for bz2 test file: FileParamTest.nt.bz2") {
-    //val df = Datafile(File("src/main/resources/FileParamTest.nt.bz2").toJava)(new SystemStreamLog())
-    val testFile = new File(getClass.getClassLoader.getResource("filestat/basic.nt.bz2").getFile)
-
-    val df = Datafile(testFile)(new SystemStreamLog())
-    df.updateFileMetrics();
+  test("basic fixed value file parameters/metadata test for bz2 test file: basic.nt.bz2") {
+    val df =datafile("filestat/basic.nt.bz2")
     assert(df.sha256sum==="1ce31e72c9553e8aa3ed63acd22f3046321a0df2d8ecb85b59af28f5bfb3cbd7" , "sha256sum is calculated wrong")
     assert(df.nonEmptyLines === 6 , "non-empty lines count is calculated wrong")
     assert(df.duplicates === 2, "duplicate lines count is calculated wrong")
     assert(df.sorted === true, "sorted lines count is calculated wrong")
-    print(df.toString)
     assert(df.bytes === 323, "bytes count is calculated wrong")
     assert(df.uncompressedByteSize === 734, "uncompressedByteSize is calculated wrong")
   }
-/*
-  test("file parameters/metadata test for plain NT test file: FileParamTest.nt") {
-    val df = Datafile(File("src/main/resources/filestats/FileParamTest.nt").toJava)(new SystemStreamLog())
-    df.updateFileMetrics();
-    assert(df.sha256sum==="c9a4f5e0aaf1b04dc9646be0fe6d47cf676d6b031d29c20bbf03ea85f450a216" , "sha256sum is calculated wrong")
-    assert(df.nonEmptyLines === 6 , "non-empty lines count is calculated wrong")
-    assert(df.duplicates === 2, "duplicate lines count is calculated wrong")
+
+  test("testing sort order US Sorted vs. ASCII") {
+    var df = datafile("filestat/sorttest_us.ttl")
+    assert(df.sorted === false, "sorted lines count is calculated wrong")
+    df  = datafile("filestat/sorttest_ascii.txt")
     assert(df.sorted === true, "sorted lines count is calculated wrong")
-    print(df.toString)
-    assert(df.bytes === 734, "bytes count is calculated wrong")
-    assert(df.uncompressedByteSize === 734, "uncompressedByteSize is calculated wrong")
+
   }
-*/
+
+  def datafile(resourcename:String): Datafile = {
+    val testFile = new File(getClass.getClassLoader.getResource(resourcename).getFile)
+
+    val df = Datafile(testFile)(new SystemStreamLog())
+    df.updateFileMetrics();
+    print(df.toString)
+    df
+
+  }
+
 }
