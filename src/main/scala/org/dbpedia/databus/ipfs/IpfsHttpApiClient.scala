b78a3dabaa511f72e2398746105146f20d6610fc
Kirill Yankov <myworkpostbox@gmail.com>
2020-06-19 19:42:05 +0200

    universal upload

--- src/main/scala/org/dbpedia/databus/ipfs/IpfsHttpApiClient.scala
+++ src/main/scala/org/dbpedia/databus/ipfs/IpfsHttpApiClient.scala
@@ -1,23 +1,78 @@
 package org.dbpedia.databus.ipfs
 
-import java.io.File
+
+import java.io.ByteArrayOutputStream
 import java.net.URI
 import java.nio.file.{Files, Path, Paths}
 import java.util.stream.Collectors
 
-
-import org.dbpedia.databus.ipfs.IpfsApiClient.FileMeta
-import scalaj.http.{BaseHttp, MultiPart}
+import org.dbpedia.databus.ipfs.IpfsApiClient.{FileMeta, MultiPart, MultipartHeader}
+import scalaj.http.{BaseHttp, PlainUrlFunc}
 
 import scala.concurrent.{Await, ExecutionContext, Future}
 import spray.json._
 
 import scala.concurrent.duration.Duration
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Random, Success}
 import collection.JavaConverters._
 
 
 object IpfsApiClient {
+
+  class Header(val name: String, val value: String)
+
+  object Header {
+    def apply(name: String, value: String): Header = new Header(name, value)
+  }
+
+  case class MultipartHeader(override val name: String, override val value: String) extends Header(name, value)
+
+  case class MultiPart(headers: Seq[MultipartHeader], data: Array[Byte]) {
+
+    import MultiPart._
+
+    def getBytes(boundary: Array[Byte]) = {
+      val arr = new ByteArrayOutputStream()
+      val sb = headers.map(h => h.name + ": " + h.value)
+        .foldLeft(StringBuilder.newBuilder)((b, r) => b.append(r).append(Newline))
+      val bytes = sb.toString().getBytes
+      arr.write("--".getBytes())
+      arr.write(boundary)
+      arr.write(Newline.getBytes)
+      arr.write(bytes)
+      arr.write(Newline.getBytes)
+      arr.write(data)
+      arr.write(Newline.getBytes())
+      arr.toByteArray
+    }
+  }
+
+  object MultiPart {
+    private val Newline = "\r\n"
+    private val random = Random.alphanumeric
+
+    def buildRequest(parts: Seq[MultiPart]): (Header, Array[Byte]) = {
+      val bnd = getBoundary()
+      val hdr = getMultipartContentType(bnd)
+      val body = new ByteArrayOutputStream()
+      parts.foreach(p => body.write(p.getBytes(bnd.getBytes())))
+      body.write("--".getBytes())
+      body.write(getBoundary().getBytes())
+      body.write("--".getBytes())
+      body.write(Newline.getBytes())
+      (hdr, body.toByteArray)
+    }
+
+    def getMultipartContentType(boundary: String): Header =
+      Header("ContentType", s"""multipart/form-data; boundary="$boundary"""")
+
+    def getBoundary(): String = {
+      val s = random.take(45).foldLeft(StringBuilder.newBuilder)((b, c) => b.append(c))
+      s.toString()
+    }
+  }
+
+
 
   object IpfsApiProtocol extends DefaultJsonProtocol {
 
@@ -36,22 +91,10 @@ class IpfsApiClient(host: String, port: Int, protocol: String)(implicit ec: Exec
   private val baseIpfsApiUrl = URI.create(s"$protocol://$host:$port/api/v0/")
   private val client = new BaseHttp()
 
-  def add(filename: String, data: Array[Byte]): Future[FileMeta] = {
-    val uri = baseIpfsApiUrl.resolve("add")
-    val req = client(uri.toURL.toString)
-
-    val mp = MultiPart("file", filename, "application/octet-stream", data)
-    // futures are needed for the ability to use async client in the future
-    Future {
-      val mpReq = req.postMulti(mp)
-      mpReq.asString.body.parseJson.convertTo[FileMeta]
-    }
-  }
-
   //todo add files without sending over the network
-  def addFolder(folder: Path, root: Path): Future[String] = {
+  def addFolder(folder: Path, root: Path, nocopy: Boolean = false): Future[String] = {
     // todo set parameters in a neat way
-    val uri = baseIpfsApiUrl.resolve("add?wrap-with-directory=true")
+    val uri = baseIpfsApiUrl.resolve(s"add?wrap-with-directory=true&nocopy=$nocopy")
     val req = client(uri.toURL.toString)
 
     val folderToAdd = root.resolve(folder)
@@ -65,16 +108,38 @@ class IpfsApiClient(host: String, port: Int, protocol: String)(implicit ec: Exec
       .map(fn => {
         val pathVal = root.relativize(fn.toPath).toString
         if (fn.isDirectory) {
-          MultiPart("file", pathVal, "application/x-directory", "")
+          val headers = Seq(
+            MultipartHeader("Content-Disposition", s"""form-data; name="file"; filename="$pathVal""""),
+            MultipartHeader("Content-Type", "application/x-directory")
+          )
+
+          MultiPart(headers, new Array[Byte](0))
         } else {
-          val data = Files.readAllBytes(root.resolve(fn.toPath))
-          MultiPart("file", pathVal, "application/octet-stream", data)
+          var headers = Seq(
+            MultipartHeader("Content-Disposition", s"""form-data; name="file"; filename="$pathVal""""),
+            MultipartHeader("Content-Type", "application/octet-stream")
+          )
+          if (nocopy) {
+            val absPath: Path = ???
+            headers = headers :+ MultipartHeader("Abspath", absPath.toString)
+          }
+
+          val data = if (nocopy) {
+            new Array[Byte](0)
+          } else {
+            Files.readAllBytes(root.resolve(fn.toPath))
+          }
+          MultiPart(headers, data)
         }
       })
 
     // futures are needed for the ability to use async client in the future
     Future {
-      val mpReq = req.postMulti(mp: _*)
+      val (hdr, bd) = MultiPart.buildRequest(mp)
+      val mpReq = req
+        .copy(method = "POST", urlBuilder = PlainUrlFunc)
+        .header(hdr.name, hdr.value)
+        .postData(bd)
       mpReq.asString.body
     }
   }
